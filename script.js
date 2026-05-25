a47fe2d2f2b154f67e5fb6202796bedaa6d9fe5c
Md Mustaqeem <mdmustaqeem820@gmail.com>
2026-05-25 23:08:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+
+//your JS code here. If required.
+
+const element = document.getElementById("level");
+
+let level = 0;
+
+let current = element;
+
+// Count parent levels
+while (current) {
+
+	level++;
+
+	current = current.parentElement;
+}
+
+// Display result
+alert("The level of the element is: " + level);
